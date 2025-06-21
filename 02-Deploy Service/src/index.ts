439e6323fb280b089e313514cf6e0471bad2fcb4
Shreyas Prabhu <shreyasprabhu26@gmail.com>
2025-06-21 20:42:23 +0530

    chore: initialize Deploy Service with TypeScript configuration and git setup

---
+++ 02-Deploy Service/src/index.ts
@@ -0,0 +1,15 @@
+import { createClient, commandOptions } from "redis";
+import { copyFinalDist, downloadS3Folder } from "./aws";
+import { buildProject } from "./utils";
+
+async function main() {
+    while (1) {
+        const res = await subscriber.brPop(
+            commandOptions({ isolated: true }),
+            'build-queue',
+            0
+        );
+        console.log(res.element)
+    }
+}
+main();
